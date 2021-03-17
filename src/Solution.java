2e28d6ef8b6e8a95b89a16d2d02b7e6afa3311d8
xinyu <xinyu.29@qq.com>
2021-03-17 21:27:04 +0800

    64. 最小路径和

--- src/Solution.java
+++ src/Solution.java
@@ -1723,6 +1723,29 @@ public class Solution {
         }
         return dp[n-1];
     }
+    public int minPathSum(int[][] grid) {
+        int m=grid.length,n=grid[0].length;
+        int[][] dp=new int[m][n];
+        dp[0][0]=grid[0][0];
+        if(m<2 && n<2) return dp[0][0];
+        if(n>1)
+            dp[0][1]=grid[0][0]+grid[0][1];
+        if(m>1)
+            dp[1][0]=grid[0][0]+grid[1][0];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i>0 && j>0){
+                    dp[i][j]=Math.min(dp[i-1][j],dp[i][j-1])+grid[i][j];
+                }else if(j>0){
+                    dp[i][j]=dp[i][j-1]+grid[i][j];
+                }else if(i>0){
+                    dp[i][j]=dp[i-1][j]+grid[i][j];
+                }
+
+            }
+        }
+        return dp[m-1][n-1];
+    }
     public static void main(String[] args) throws Exception {
         System.out.println(climbStairs(3));
 
